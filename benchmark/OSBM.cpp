e1b021da2e6a2cd8a081374e780e366bc489c067
KaiwenHe <kaiwen.he7@gmail.com>
2022-08-17 16:28:45 -0700

    OSBM works! Need to implement for format where V is a 1-d array

--- benchmark/OSBM.cpp
+++ benchmark/OSBM.cpp
@@ -9,6 +9,11 @@
 #include <time.h>
 #include <stdlib.h>
 
+template<typename T, size_t rows, size_t cols>
+void print_mat(double M[rows][cols]); 
+
+template <typename T> int sgn(T val);
+
 template<typename T, int64_t n_rows, int64_t n_cols>
 void OSBM(T (*V)[n_rows][n_cols], T (*lev)[n_cols]) {
     int its = 0;               /* Records number of iterations to convergence */
@@ -18,13 +23,13 @@ void OSBM(T (*V)[n_rows][n_cols], T (*lev)[n_cols]) {
     T r_ii, r_jj, r_ij;     /* values used to compute cos and sin for the givens rotation */
     T t, cos, sin;           
     T colnorms[n_cols];     /* Array to hold row norms of V at each iteration */
-    T coltemp[n_rows];
+    T coltemp[n_rows], coltemp2[n_rows]; /* Temporary arrays to store col of V */
     
     while(true) {
         cond = false;
         ccond = true;
 
-        for (a=0; a<n_cols; a++){
+        for (a=0; a<n_cols; a++){      /* updates colnorms to store col norms of V at each iteration */
             for (b=0; b<n_rows; b++){
                 coltemp[b] = (*V)[b][a];
             }
@@ -32,7 +37,7 @@ void OSBM(T (*V)[n_rows][n_cols], T (*lev)[n_cols]) {
         }
 
 
-        for (a=0; a<(n_cols-1); a++) {
+        for (a=0; a<(n_cols-1); a++) {    /* OSBM conditional to choose indices i,j to perform givens rotations on */
             for (b=a+1; b<n_cols; b++) {
                 if (((*lev)[a] - colnorms[a] > 1e-10) && (colnorms[b] - (*lev)[b] > 1e-10)) {
                     ccond = true;
@@ -55,10 +60,43 @@ void OSBM(T (*V)[n_rows][n_cols], T (*lev)[n_cols]) {
                 break;
             }    
         } 
-       
 
-        std::cout << i << "   " << j << '\n';
-        break; 
+        if (cond==false || its==n_cols) {
+            break;
+        }
+
+        for (a=0;a<n_rows;a++) {         /*stores col i and col j to perform dot product on for r_ij*/
+            coltemp[a] = (*V)[a][i];
+            coltemp2[a] = (*V)[a][j];
+        }  
+
+        r_ii = colnorms[i];
+        r_jj = colnorms[j];
+        r_ij = blas::dot(n_rows, coltemp, 1, coltemp2, 1);
+
+        if ((*lev)[i] - r_ii < r_jj - (*lev)[j]) {
+            t = (r_ij + sqrt(pow(r_ij,2) - (r_ii - (*lev)[i])*(r_jj - (*lev)[i]))) / (r_jj - (*lev)[i]);
+            cos = 1/sqrt(1 + pow(t,2));
+            sin = cos*t;
+            blas::rot(n_rows, coltemp, 1, coltemp2, 1, cos, sin);
+            for (a=0;a<n_rows;a++){
+                (*V)[a][i] = coltemp[a];
+                (*V)[a][j] = coltemp2[a];
+            }
+        } else {
+            t = (-r_ij - sqrt(pow(r_ij,2) - (r_ii -  (*lev)[j])*(r_jj - (*lev)[j]))) / (r_ii - (*lev)[j]);
+            cos = 1/sqrt(1 + pow(t,2));
+            sin = cos*t;
+            blas::rot(n_rows, coltemp, 1, coltemp2, 1, cos, sin);
+            for (a=0;a<n_rows;a++){
+                (*V)[a][i] = coltemp[a];
+                (*V)[a][j] = coltemp2[a];
+            }
+        }
+
+        its += 1;
+        cond = false;
+
     }
 }
 
@@ -67,7 +105,24 @@ int main( int argc, char *argv[] ) {
     double V[3][5] = {{0,0,1,0,0}, {0,0,0,1,0}, {0,0,0,0,1}};
     double ell[5] = {0.4, 0.5, 0.6, 0.7, 0.8};
     OSBM<double,3,5>(&V, &ell);
+    print_mat<double,3,5>(V);
 }
 
 
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
 
+
+template<typename T, size_t rows, size_t cols>
+void print_mat(double M[rows][cols]) {
+    for (int i=0;i<rows;i++){
+        std::cout << std::endl;
+        std::cout << "|";
+        for (int k=0; k<cols; k++){
+            if(k>0) std::cout << "|";
+            std::cout << M[i][k];
+            if(k==cols-1) std::cout << "|";
+        }
+    }
+}
--- benchmark/test.cpp
+++ benchmark/test.cpp
@@ -10,6 +10,9 @@
 #include <stdlib.h>
 #include <chrono>
 
+template<typename T, size_t rows, size_t cols>
+void print_mat(double M[rows][cols]);
+
 template <typename T, size_t rows, size_t cols>
 void printval(T (*array)[rows][cols]){
     int i,j;
@@ -17,24 +20,58 @@ void printval(T (*array)[rows][cols]){
     T rowtemp[cols];
     /**array[0][0] = 1.0;
     std::cout << *array[0][1] << '\n';*/
-    for (i=0; i<rows; i++) {
+    /*for (i=0; i<rows; i++) {
         for (j=0; j<cols; j++) {
             rowtemp[j] = (*array)[i][j];
-            /*std::cout << (*array)[i][j] << '\n';*/
+            std::cout << (*array)[i][j] << '\n';
         }
         rownorms[i] = blas::dot(cols,rowtemp,1,rowtemp,1);
     }
 
     for (i=0;i<rows;i++) {
         std::cout << rownorms[i] << '\n';
-    }
+    }*/
+    /*double* critrow = (*array)[2];*/
+    std::cout << array[0][1] << "\n";
+}
+
+template<typename T, size_t len>
+void rot(T (*array)[len]){
+    blas::rot(4, array[1], 1, array[2], 1, 0.1, 0.8);
 }
 
 
 int main( int argc, char *argv[] ) {
+    int i;
 
-    double V[3][4] = {{0,1,2,3}, {4,5,6,7}, {8,9,10,11}};
-    printval<double,3,4>(&V);
+    /*double V[3][4] = {{0,1,2,3}, {4,5,6,7}, {8,9,10,11}};*/
+    double V[12] = {0,1,2,3,4,5,6,7,8,9,10,11};
+    /*rot<double,12>(&V);*/
+    /*for (i=0; i<10;i++){
+        std::cout << V[i] << "\n";
+    }*/
     /*std::cout << V[0][0];*/
+    /*double test[4] = {0,1,2,3};
+    double test2[4] = {8,9,10,11};
+    blas::rot(4,(&test)[0],1,(&test2)[0],1,0.1,0.8);
+    std::cout << test2[3];*/
+    blas::rot(4,(&V)[0],1,(&V)[5],1,0.1,0.8);
+    std::cout << V[0];
+
 
 }
+
+template<typename T, size_t rows, size_t cols>
+void print_mat(double M[rows][cols]) {
+    for (int i=0;i<rows;i++){
+        std::cout << std::endl;
+        std::cout << "|";
+        for (int k=0; k<cols; k++){
+            if(k>0) std::cout << "|";
+            std::cout << M[i][k];
+            if(k==cols-1) std::cout << "|";
+        }
+    }
+}
+
+
